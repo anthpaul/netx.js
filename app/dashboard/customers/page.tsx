e22dc5332b38c46d4b8686fb2af337b60d48aaf0
anthpaul <pauldpiguave@gmail.com>
2026-06-03 20:13:04 -0500

    ultimo commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,11 +6,10 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { query?: string };
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const customers = await fetchFilteredCustomers(query);
 
